ebde6b480676077092096ff2618cd15f868a79cc
Maksim <maks.s375@gmail.com>
2022-10-05 11:26:53 +0300

    add create user

---
+++ server/models/user.js
@@ -0,0 +1,42 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+  
+     
+    static associate(models) {
+     User.hasMany(models.Task,{
+       foreignKey:'userId'
+     })
+    }
+  }
+  User.init({
+    login: {
+      allowNull:false,
+      type: DataTypes.STRING(64),
+      unique:true,
+      validate:{
+        notEmpty:true,
+        notNull:true
+      },
+      set(v){
+        this.setDataValue('password','hash');
+      }
+    },
+    password: {
+      field:'password_hash',
+      type:DataTypes.TEXT,
+    },
+    avatar: {
+      type:DataTypes.TEXT,
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+    tableName:'users',
+    underscored:true
+  });
+  return User;
+};
---
+++ server/routes/tasksRouter.js
@@ -0,0 +1,6 @@
+const {Router} = require('express');
+
+const tasksRouter = Router();
+
+
+module.exports = tasksRouter;
